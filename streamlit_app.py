7084fcd03418b6a871e3446c44c2f26d5f36bfc5
bhaktiram <127795073+bhaktiram@users.noreply.github.com>
2023-03-13 21:59:12 +0000

    Update streamlit_app.py
    added breakfast menu card details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parent New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & bloobery oatmeal')
+streamlit.text('Kale, Spinach, Rocket Smoothie')
+streamlit.text('Hard boiled free range egg')
