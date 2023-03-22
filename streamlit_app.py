1718cf5555cfc94618dc85fefccb9bc4570cba03
bhaktiram <127795073+bhaktiram@users.noreply.github.com>
2023-03-22 22:46:14 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,17 +74,30 @@ streamlit.header("Fruityvice Fruit Advice!")
 # dont run anything past here
    #streamlit.stop
 
-import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-#my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-#streamlit.text("The Fruitload Container")
+#normal code without button
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+##my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+##my_data_row = my_cur.fetchone()
+#my_data_rows = my_cur.fetchall()
+##streamlit.text("The Fruitload Container")
+#streamlit.header("The Fruitload Container")
+##streamlit.text(my_data_row)
+#streamlit.dataframe(my_data_rows)
+
+#using button
 streamlit.header("The Fruitload Container")
-#streamlit.text(my_data_row)
-streamlit.dataframe(my_data_rows)
+def get_fuit_load_list():
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+         return my_cur.fetchone()
+#add a button to fruit load list
+if streamlit.button("Get the Fruit Load list")
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fuit_load_list()
+    streamlit.dataframe(my_data_rows)
+
 
 ## adding new table and showing what user has selected in text
 
